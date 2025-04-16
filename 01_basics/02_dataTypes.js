90fcbd62fed8d743846662abe8bbbf80b375c514
rdxpt <rdxpt@outlook.com>
2025-04-16 18:00:39 +0530

    1/20 02_dataTypes.js done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hello") // we are using nodejs and not browser
+// avoid semicolons
+
+let name = "rdxpt"
+let age = 20
+let isLoggedIn = true
+
+// number => 2^52/53 range
+// bigint
+// string ""
+// boolean true/false
+// null => standalone value
+// undefined => no value exists (diff than null)
+// symbol  => unique
+
+// object 
+
+console.log(typeof "rdxpt")     // string
+console.log(typeof null)        // object
+console.log(typeof undefined)   // undefined
